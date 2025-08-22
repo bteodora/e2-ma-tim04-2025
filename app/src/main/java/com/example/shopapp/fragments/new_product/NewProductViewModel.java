4e04c1d96e5137c494310b2c6a5cfab32a090196
bteodora <149783651+bteodora@users.noreply.github.com>
2025-08-22 16:01:16 +0200

    removed imported project

--- app/src/main/java/com/example/shopapp/fragments/new_product/NewProductViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopapp.fragments.new_product;
-
-import androidx.lifecycle.ViewModel;
-
-public class NewProductViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
