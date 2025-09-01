862ed55b29f77976c3def49081e5320d17948956
bteodora <149783651+bteodora@users.noreply.github.com>
2025-09-01 12:30:24 +0200

    feat: User stays logged in after leaving the app

--- app/src/main/java/com/example/rpgapp/activities/AuthActvity.java
+++ app/src/main/java/com/example/rpgapp/activities/AuthActvity.java
@@ -1,21 +1,22 @@
 package com.example.rpgapp.activities;
 
+import android.content.Intent; // <-- VAŽAN IMPORT
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.example.rpgapp.R;
+import com.google.firebase.auth.FirebaseAuth; // <-- VAŽAN IMPORT
 
 public class AuthActvity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Samo postavi layout. Ništa više nije potrebno.
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(AuthActvity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         setContentView(R.layout.activity_auth);
     }
 }
